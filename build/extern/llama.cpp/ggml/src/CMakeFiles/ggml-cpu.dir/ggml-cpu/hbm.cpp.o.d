80becdad910cae494cadd3ba9975ec9710d0ef3b
24birajpokhrel <24birajpokhrel@gmail.com>
2026-08-01 10:02:14 -0500

    Fixining gitignore by adding more things, and getting build oout of repo

--- build/extern/llama.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o.d
+++
@@ -1,3 +0,0 @@
-extern/llama.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o: \
- /home/drac/gitRepos/LectureHelp/extern/llama.cpp/ggml/src/ggml-cpu/hbm.cpp \
- /usr/include/stdc-predef.h
--- build/src/CMakeFiles/lecturehelp.dir/ASR.cpp.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/lecturehelp.dir/ASR.cpp.o: \
- /home/drac/gitRepos/LectureHelp/src/ASR.cpp /usr/include/stdc-predef.h
